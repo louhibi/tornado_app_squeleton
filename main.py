73b648ad9adade7f56658ec2785ba7f0b0d0a740
louhibi <louhibi@gmail.com>
2013-11-15 09:34:57 -0500

    multiple apps

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import logging
 
 ##internal imports
 from myApp.app import MyApplication
+from myApp1.app import MyApplication1
 
 
 
@@ -23,7 +24,8 @@ def main():
         """
         defining/parsing the options
         """
-        define("port", default=8082, help="run on the given port", type=int)
+        define("port", default=8080, help="run on the given port", type=int)
+        define("port1", default=8081, help="run on the given port", type=int)
         define("debug", default=False, help="run in debug mode", type=bool)
         tornado.options.parse_command_line()        
         logging.debug(options.logging)
@@ -31,14 +33,18 @@ def main():
         initalising applications
         """ 
         app = MyApplication(options)
+        app1 = MyApplication1(options)
 
         """
         starting tornado server
         """
         http_server = tornado.httpserver.HTTPServer(app)
         http_server.listen(options.port)
+        http_server1 = tornado.httpserver.HTTPServer(app1)
+        http_server1.listen(options.port1)
         ioloop = tornado.ioloop.IOLoop.instance()
         tornado.autoreload.add_reload_hook(app.reloadTasks)
+        tornado.autoreload.add_reload_hook(app1.reloadTasks)
         tornado.autoreload.start(ioloop)
         ioloop.start()
 
